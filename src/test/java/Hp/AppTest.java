cf40f71c346efbba68f2d71a29c86ac0dde92f0b
vijay <vijayalampur06@yahoo.com>
2022-05-20 12:35:56 +0530

    this is laptop config

---
+++ src/test/java/Hp/AppTest.java
@@ -0,0 +1,20 @@
+package Hp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
